d4b56069f95ee97ecf60379b4754c4234e392bab
Sujatabatra <sujatabatra@hotmail.com>
2021-03-24 12:51:03 +0530

    Add files via upload

---
+++ Day2/MyGreetDemoProject/src/com/sujata/consumer/ClientMain.java
@@ -0,0 +1,25 @@
+package com.sujata.consumer;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import com.sujata.producer.GoodMorning;
+import com.sujata.producer.Greet;
+
+public class ClientMain {
+
+	public static void main(String[] args) {
+//		Not the spring way
+//		Greet greet=new GoodMorning();//creating of object and life cycle of component
+//		greet.wish("Sujata");
+		
+		//Spring Container : managing life cycle(Object Creating/constructor calling,calling setter) of all the means mentioned in XML file
+		ApplicationContext springContainer=new ClassPathXmlApplicationContext("greet.xml");
+		
+		Greet greet=(Greet)springContainer.getBean("eve");
+		
+		greet.wish();
+		
+	}
+
+}
---
+++ Day2/MyGreetDemoProject/src/com/sujata/producer/GoodMorning.java
@@ -0,0 +1,16 @@
+package com.sujata.producer;
+
+public class GoodMorning implements Greet {
+
+	private String name;
+	
+	public void setName(String name) {
+		this.name = name;
+	}
+	@Override
+	public void wish() {
+		System.out.println("Good Morning "+name);
+
+	}
+
+}
